58f194e9d6587c0d92714e51eb3bbbc40758979b
Missangela99 <angelikahardyn99@gmail.com>
2024-06-21 16:33:31 +0200

    Fully functional calculator, every button, apart from the decimal point and changing number from negative to positive works.

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-function add(num1, num2) {
-    return num1 + num2;
+function add(number1, number2) {
+    result = number1 + number2;
+    display.innerHTML = `${result}`
 };
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    result = num1 - num2;
+    display.innerHTML = `${result}`
 };
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    result = num1 * num2;
+    display.innerHTML = `${result}`
 };
 
 function divide(num1, num2) {
-    return num1 / num2;
+    result = num1 / num2;
+    display.innerHTML = `${result}`
 };
 
+let result = 0;
 let newArray = [];
-let num1 = '';
-let num2 = '';
+let num1;
+let num2;
 let mathOperator = '';
+
 const display = document.querySelector(".display");
 const number = document.querySelectorAll(".btn-number");
 const clear = document.querySelector(".backspace");
@@ -25,19 +31,63 @@ const operation = document.querySelectorAll(".right-buttons");
 const equal = document.querySelector(".equal");
 const allClear = document.querySelector(".AC");
 
-console.log(number)
-
 number.forEach(button => {
     let numberValue = button.innerHTML;
     button.addEventListener("click", () => {
         display.innerHTML += numberValue;
         newArray.push(numberValue);
-
-        if (num1) {
-            display.innerHTML = ''
-        }
-
     });
+});
+
+function getNum() {
+    if (typeof num1 != `number`) {
+        num1 = newArray.join('');
+        num1 = parseFloat(num1);
+        newArray = [];
+        display.innerHTML = '';
+
+    } else if (typeof num1 == `number`) {
+        num2 = newArray.join('');
+        num2 = parseFloat(num2);
+        newArray = [];
+        display.innerHTML = '';
+
+    } else if (typeof num1 == `number` && typeof num2 == `number`) {
+        //nothing
+    }
+}
+
+operation.forEach(operator => {
+    operator.addEventListener("click", () => {
+        getNum();
+        mathOperator = operator.innerHTML;
+    });
+});
+
+
+
+equal.addEventListener("click", () => {
+    getNum();
+    if (mathOperator == '+') {
+        add(num1, num2);
+        num1 = result;
+        num2 = undefined;
+
+    } else if (mathOperator == '-') {
+        subtract(num1, num2);
+        num1 = result;
+        num2 = undefined;
+
+    } else if (mathOperator == '/') {
+        divide(num1, num2);
+        num1 = result;
+        num2 = undefined;
+
+    } else if (mathOperator == '*') {
+        multiply(num1, num2);
+        num1 = result;
+        num2 = undefined;
+    }    
 });
 
 
@@ -46,21 +96,9 @@ clear.addEventListener("click", () => {
     newArray.pop();
 });
 
-operation.forEach(operator => {
-    operator.addEventListener("click", () => {
-        if (newArray.length !== 0) {
-            num1 = newArray.join("");
-            display.innerHTML += operator.innerHTML
-            newArray = [];
-        };
-    });
-});
-
 allClear.addEventListener("click", () => {
     display.innerHTML = '';
-    num1 = 0;
-    num2 = 0;
+    num1 = [];
+    num2 = [];
     newArray = [];
 });
-
-
