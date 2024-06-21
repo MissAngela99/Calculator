ff208ec1a2beaa1e72d6e0795550ea239849ae4c
Missangela99 <angelikahardyn99@gmail.com>
2024-06-21 15:13:37 +0200

    Made logic to only print numbers and operators to the display, AC button clears everything off the screen and variables

--- script.js
+++ script.js
@@ -15,9 +15,15 @@ function divide(num1, num2) {
 };
 
 let newArray = [];
+let num1 = '';
+let num2 = '';
+let mathOperator = '';
 const display = document.querySelector(".display");
 const number = document.querySelectorAll(".btn-number");
 const clear = document.querySelector(".backspace");
+const operation = document.querySelectorAll(".right-buttons");
+const equal = document.querySelector(".equal");
+const allClear = document.querySelector(".AC");
 
 console.log(number)
 
@@ -26,6 +32,11 @@ number.forEach(button => {
     button.addEventListener("click", () => {
         display.innerHTML += numberValue;
         newArray.push(numberValue);
+
+        if (num1) {
+            display.innerHTML = ''
+        }
+
     });
 });
 
@@ -34,3 +45,22 @@ clear.addEventListener("click", () => {
     display.innerHTML = display.innerHTML.slice(0, -1);
     newArray.pop();
 });
+
+operation.forEach(operator => {
+    operator.addEventListener("click", () => {
+        if (newArray.length !== 0) {
+            num1 = newArray.join("");
+            display.innerHTML += operator.innerHTML
+            newArray = [];
+        };
+    });
+});
+
+allClear.addEventListener("click", () => {
+    display.innerHTML = '';
+    num1 = 0;
+    num2 = 0;
+    newArray = [];
+});
+
+
