36c1873a0f47a900caef9fe56e077b596b130b7b
Missangela99 <angelikahardyn99@gmail.com>
2024-06-22 12:46:50 +0200

    rewriting entire calculator logic

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function add(number1, number2) {
-    result = number1 + number2;
+function add(num1, num2) {
+    result = num1 + num2;
     display.innerHTML = `${result}`
 };
 
@@ -15,8 +15,8 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     result = num1 / num2;
-    display.innerHTML = `${result}`
-};
+    display.innerHTML = `${result}` 
+};  
 
 let result = 0;
 let newArray = [];
@@ -31,78 +31,5 @@ const operation = document.querySelectorAll(".right-buttons");
 const equal = document.querySelector(".equal");
 const allClear = document.querySelector(".AC");
 
-number.forEach(button => {
-    let numberValue = button.innerHTML;
-    button.addEventListener("click", () => {
-        display.innerHTML += numberValue;
-        newArray.push(numberValue);
-    });
-});
-
-function operate() {
-    if (mathOperator == '+') {
-        add(num1, num2);
-        num1 = result;
-        num2 = undefined;
-
-    } else if (mathOperator == '-') {
-        subtract(num1, num2);
-        num1 = result;
-        num2 = undefined;
-
-    } else if (mathOperator == '/') {
-        divide(num1, num2);
-        num1 = result;
-        num2 = undefined;
-
-    } else if (mathOperator == '*') {
-        multiply(num1, num2);
-        num1 = result;
-        num2 = undefined;
-    }    
-}
-
-function getNum() {
-    if (typeof num1 != `number`) {
-        num1 = newArray.join('');
-        num1 = parseFloat(num1);
-        newArray = [];
-        display.innerHTML = '';
-
-    } else if (typeof num1 == `number`) {
-        num2 = newArray.join('');
-        num2 = parseFloat(num2);
-        newArray = [];
-        display.innerHTML = '';
-
-    };
-};
-
-operation.forEach(operator => {
-    operator.addEventListener("click", () => {
-        getNum();
-        mathOperator = operator.innerHTML;
-        if (typeof num1 == `number` && typeof num2 == `number`) {
-            display.innerHTML = '';
-            operate();
-        }
-    });
-});
-
-equal.addEventListener("click", () => {
-    getNum();
-    operate();
-});
 
 
-clear.addEventListener("click", () => {
-    display.innerHTML = display.innerHTML.slice(0, -1);
-    newArray.pop();
-});
-
-allClear.addEventListener("click", () => {
-    display.innerHTML = '';
-    num1 = [];
-    num2 = [];
-    newArray = [];
-});
