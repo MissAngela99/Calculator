8983edb9a6d84f613158091052b2111bb749c500
Missangela99 <angelikahardyn99@gmail.com>
2024-06-22 21:34:57 +0200

    -

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ let num1 = null;
 let num2 = null;
 let mathOperator = null;
 
+
 const display = document.querySelector(".display");
 const number = document.querySelectorAll(".btn-number");
 const clear = document.querySelector(".backspace");
@@ -35,47 +36,42 @@ const equal = document.querySelector(".equal");
 const allClear = document.querySelector(".AC");
 
 number.forEach(button => {
-    let value = button.innerHTML;
+    let value = button.textContent;
         button.addEventListener("click", () => {
-        display.innerHTML += value;
-        array.push(value);
-
-        if (result != null && num1 != null) {
-            display.innerHTML = value;
-            mathOperator = null;
-            num1 = null;
-            result = null;
-        }
-    })
+            display.textContent += value;
+            array.push(value);
+    });
 });
 
 
 operation.forEach(operator => {
+
     operator.addEventListener("click", () => {
         if (num1 == null) {
             num1 = parseFloat(getNumber());
-            display.innerHTML = operator.innerHTML;
+            display.textContent = operator.innerHTML;
             mathOperator = operator.innerHTML;
 
         } else {
-            display.innerHTML = operator.innerHTML;
+            display.textContent = operator.innerHTML;
             mathOperator = operator.innerHTML;
         }
-        
-        return mathOperator;
+
     });
 });
 
 
 equal.addEventListener("click", () => {
-    if (num2 == null) {
-        num2 = parseFloat(getNumber());
-        operate(num1, num2);
-        display.innerHTML = result;
-        control = result + 1;
+    if (num2 == null || num1 != null) {
 
+        num2 = parseFloat(getNumber());
+        operate(num1, num2);        
+
+        display.textContent = result;
         num1 = result;
         num2 = null;
+        result = null;
+        control = 'present';
     };
 });
 
